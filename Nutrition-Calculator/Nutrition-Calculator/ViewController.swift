4845af28591154115731fc5480f53a9bbd6d0746
Mim0sa <mim0sa@qq.com>
2020-08-01 10:20:13 +0800

    initial project commit

---
+++ Nutrition-Calculator/Nutrition-Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nutrition-Calculator
+//
+//  Created by 吉乞悠 on 2020/8/1.
+//  Copyright © 2020 吉乞悠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
