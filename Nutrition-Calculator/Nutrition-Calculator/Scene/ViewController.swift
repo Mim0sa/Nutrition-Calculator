742553430355331f01c3e550bcfe0bce8b3a9b88
mimosa <563576965@qq.com>
2020-08-04 16:37:32 +0800

    finish 汉堡 category in plist

--- Nutrition-Calculator/Nutrition-Calculator/Scene/ViewController.swift
+++ Nutrition-Calculator/Nutrition-Calculator/Scene/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         fetchMenuFromPlist()
         
@@ -22,8 +21,7 @@ class ViewController: UIViewController {
             print(food.name)
         }
     }
-
-
+    
 }
 
 extension ViewController {
