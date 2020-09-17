8b47231d475f061d0cb6c22927f224ed1cc8d3b0
mimosa <563576965@qq.com>
2020-09-17 14:35:21 +0800

    add new project MDNC

---
+++ MDNC/MDNC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDNC
+//
+//  Created by Mimosa on 2020/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
